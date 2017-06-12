c23e47c34d1ddc4b8b72bce47e1ce26647cf6a7d
zhujy <zhujy@lianchuang.com>
2017-06-12 16:50:47 +0800

    router 修改

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
-// import List from '@/components/List'
-// import SettleList from '@/components/SettleList'
-// import overallChart from '@/components/overallChart'
-// import compareChart from '@/components/compareChart'
-// import workerVerify from '@/components/workerVerify'
-// import workerManage from '@/components/workerManage'
+import bonusList from '@/components/List'
+import settleList from '@/components/SettleList'
+import overallChart from '@/components/overallChart'
+import compareChart from '@/components/compareChart'
+import workerVerify from '@/components/workerVerify'
+import workerManage from '@/components/workerManage'
 
 Vue.use(Router)
 
-const bonusList = resolve => require(['@/components/List.vue'], resolve)
-const settleList = resolve => require(['@/components/settleList.vue'], resolve)
-const overallChart = resolve => require(['@/components/overallChart.vue'], resolve)
-const compareChart = resolve => require(['@/components/compareChart.vue'], resolve)
-const workerVerify = resolve => require(['@/components/workerVerify.vue'], resolve)
-const workerManage = resolve => require(['@/components/workerManage.vue'], resolve)
+// const bonusList = resolve => require(['@/components/List.vue'], resolve)
+// const settleList = resolve => require(['@/components/settleList.vue'], resolve)
+// const overallChart = resolve => require(['@/components/overallChart.vue'], resolve)
+// const compareChart = resolve => require(['@/components/compareChart.vue'], resolve)
+// const workerVerify = resolve => require(['@/components/workerVerify.vue'], resolve)
+// const workerManage = resolve => require(['@/components/workerManage.vue'], resolve)
 
 export default new Router({
   routes: [
